11575df826361a959dbbb5f84a037b2dfb21e81b
Maximilian Kerp <make@taktsoft.com>
2018-04-27 20:42:18 +0200

    Start specing out the interface

--- test/ccm.test.js
+++ test/ccm.test.js
@@ -8,4 +8,31 @@ describe('CCM', function () {
       expect(ccm.version()).to.eql('16.3.3')
     })
   })
+
+  describe('.orbit()', function() {
+
+    describe('Creating an orbit docstore', function (){
+      describe('.orbti(store: "< name >", callback)', function (){
+
+        it('creates a docstore')
+      })
+
+    })
+
+    describe('Replicating an orbit docstore', function (){
+
+    })
+
+    describe('Creating an orbit docstore', function (){
+
+    })
+
+    describe('Creating an orbit docstore', function (){
+
+    })
+  })
+
+  describe('.ipfs()', function () {
+
+  })
 })
