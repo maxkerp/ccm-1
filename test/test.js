855bcb2e0bebe9e6b11869f4843acd693d4cdf6b
Maximilian Kerp <make@taktsoft.com>
2018-04-27 12:06:20 +0200

    Create npm project structure

---
+++ test/test.js
@@ -0,0 +1,15 @@
+
+const expect = require('chai').expect;
+
+describe('First Spec', function () {
+
+  it('fails instantly', function (){
+
+    expect(2).to.equal(1)
+  })
+
+  it('is successful', function (){
+
+    expect(2).to.eql(2)
+  })
+})
