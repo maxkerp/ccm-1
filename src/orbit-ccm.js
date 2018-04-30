4377b51f2efa3c26b65e639ebafa290eec3e4f77
Maximilian Kerp <make@taktsoft.com>
2018-04-30 19:57:45 +0200

    Add Stores, restructure files

--- src/orbit-ccm.js
+++ src/orbit-ccm.js
@@ -1,54 +1,174 @@
 
-// Create private namespace
-(async function () {
-  // Create alias
-  const put = DEBUG.log
-  const wait = DEBUG.wait
+// Create Module
+var  CCM = CCM || {};
 
-  const config = {
-    EXPERIMENTAL: {
-      pubsub: true
+// Utils
+(function () {
+
+  Utils = {
+
+    log: function (string) {
+      var messages = document.querySelector('#messages'),
+          code     = document.createElement('pre'),
+          log      = document.createElement('p')
+
+      code.textContent = string
+      messages.appendChild(log.appendChild(code))
     },
-  }
 
-  window.ipfs  = new Ipfs(config)
+    wait: function () {
 
-  ipfs.on('ready', () => {
-    put("IPFS node ready.")
+      return new Promise((resolve, _)  => {
 
-    window.orbit = new OrbitDB(ipfs)
-    put("Orbit successfully created.")
+        setTimeout(function () {
+          resolve()
+        }, 5000, 'foo');
+      });
+    }
+  };
 
-    put('Initialising..')
-    // init()
-  })
+  CCM.Utils = Utils
+})();
 
-  function init() {
-    // Create aliases
-    const IPFS  = window.ipfs
-    const ORBIT = window.orbit
-    const CCM   = window.ccm
+// Create aliases
+const put  = CCM.Utils.log;
+const wait = CCM.Utils.wait;
 
-    class OrbitWrapper {
+// Store Class
+(function () {
 
-      store(name, cb) {
-        this._store(name).then( (store) => cb(store) )
-      }
+  // Object Store
+  class Store {
 
-      async _store(name) {
-
-        let store = await ORBIT.docs(name)
-        await store.load();
-
-        put(`Store ${name} created and loaded.`)
-        return store
-      }
+    constructor() {
+      this.data = {}
     }
 
-    window.wrapper = new OrbitWrapper();
+    get(key, cb) {
+      let value = this.data[key]
+
+      return cb ? cb(value) : value;
+    }
+
+    set(doc, cb) {
+      this.data[doc.key] = doc
+
+      return cb ? cb(doc) : doc;
+    }
+
+    del(key, cb) {
+      let doc = this.data[key]
+      delete this.data[key]
+
+      return cb ? cb(doc) : doc;
+    }
+
+    length() {
+      return Object.keys(this.data).length
+    }
   }
 
-})()
+  // Orbit Store
+  class OrbitStore {
+
+    constructor(docs) {
+      this.docs = docs
+    }
+
+    get(key, cb) {
+      let value = this.docs.get(key).pop()
+      console.log(value)
+
+      return cb ? cb(value) : value;
+    }
+
+    set(doc, cb) {
+      this.docs.put(doc)
+
+      return cb ? cb(doc) : doc;
+    }
+
+    del(key, cb) {
+      let doc = this.data[key]
+      delete this.data[key]
+
+      return cb ? cb(doc) : doc;
+    }
+
+    length() {
+      return Object.keys(this.data).length
+    }
+  }
 
 
+  CCM.Stores = {
+    Object: Store,
+    Orbit: OrbitStore
+  };
+})();
+
+// Wrapper
+(function (){
+
+  class OrbitWrapper {
+
+    constructor(orbitInstance) {
+      this._orbit = orbitInstance
+    }
+
+    create(options, cb) {
+      let docs = this._orbit.docs(options.name, { indexBy: 'key' })
+
+      docs.then(async function (docs) {
+
+        await docs.load()
+
+        return docs;
+      }).then(function (docs) {
+        let store = new CCM.Stores.Orbit(docs)
+
+        return cb(store)
+      })
+    }
+
+    static init(cb) {
+      if (CCM.orbit) {
+
+        console.log("Orbit module has already been initialized.")
+        return;
+      }
+
+      const config = {
+        EXPERIMENTAL: {
+          pubsub: true
+        }
+      }
+      const node = new Ipfs(config)
+
+      node.on('ready', () => {
+        put("IPFS node ready..")
+
+        const ORBIT = new OrbitDB(node)
+        put("Orbit successfully created..")
+
+        CCM.orbit = new OrbitWrapper(ORBIT)
+        put("Orbit module initialized")
+
+        if (cb) { cb() }
+      })
+    }
+  }
+
+  CCM.Orbit = OrbitWrapper
+})();
+
+// Main
+(function () {
+
+  CCM.Orbit.init(function () {
+
+    put('Ready to use Orbit module.')
+  });
+
+})();
 
--- test/ccm.test.js
+++ test/ccm.test.js
@@ -17,12 +17,154 @@ describe('CCM', function () {
 
         it('creates a docstore')
       })
-
     })
-
   })
+
 
   describe('.ipfs()', function () {
 
+  })
+})
+
+describe('Stores', function () {
+
+  describe('Object Store', function () {
+    var store;
+
+    before
+
+    beforeEach( function () {
+      store = new CCM.Stores.Object()
+      data = {
+        '001': {key: '001', name: 'max', age: 27},
+        '003': {key: '003', name: 'felix', age: 15},
+        '002': {key: '002', name: 'daniel', age: 24}
+      }
+
+      store.data = data
+
+    }) 
+
+    describe('get', function () {
+
+      it('returns value for specified key', function () {
+
+        expect(store.get('001')).to.eql({key: '001', name: 'max', age: 27})
+        expect(store.get('003')).to.eql({key: '003', name: 'felix', age: 15})
+      })
+    })
+
+    describe('set', function () {
+
+      it('sets Object with specified key', function () {
+        var new_doc = { key: 'new_key', name: 'Foo', age: null }
+
+        store.set(new_doc)
+        console.log(data)
+
+        expect(store.get(new_doc.key)).to.eql(new_doc)
+      })
+
+      it('increases the length of the store by 1', function () {
+        var new_doc = { key: 'new_key', name: 'Foo', age: null }
+
+        store.set(new_doc)
+        console.log(data)
+
+        expect(store.length()).to.eql(4)
+      })
+    })
+
+    describe('del', function () {
+
+      it('deletes Object with specified key', function () {
+        console.log(data)
+
+        store.del('002')
+        expect(store.get('002')).to.be.undefined
+      })
+
+
+      it('decreases the length of the store by 1', function () {
+        console.log(data)
+
+        store.del('002')
+        expect(store.length()).to.eql(2)
+      })
+    })
+  })
+
+  describe.only('Orbit Store', function () {
+    var store;
+
+    before(function (done){
+
+      CCM.Orbit.init(function (){
+
+        done();
+      })
+    })
+
+    beforeEach(function (done) {
+
+      CCM.orbit.create({name: 'test_store'}, function (test_store) {
+        store = test_store
+
+        store.set({key: '001', name: 'max', age: 27}, function (){
+          done();
+        });
+      })
+    }) 
+
+    describe('get', function (done) {
+
+      it('returns value for specified key', function (done) {
+
+        store.get('001', function (res) {
+
+          expect(res).to.eql({ key: '001', name: 'max', age: 27 })
+          done()
+        })
+      })
+    })
+
+    describe('set', function () {
+
+      it('sets Object with specified key', function () {
+        var new_doc = { key: 'new_key', name: 'Foo', age: null }
+
+        store.set(new_doc)
+        console.log(data)
+
+        expect(store.get(new_doc.key)).to.eql(new_doc)
+      })
+
+      it('increases the length of the store by 1', function () {
+        var new_doc = { key: 'new_key', name: 'Foo', age: null }
+
+        store.set(new_doc)
+        console.log(data)
+
+        expect(store.length()).to.eql(4)
+      })
+    })
+
+    describe('del', function () {
+
+      it('deletes Object with specified key', function () {
+        console.log(data)
+
+        store.del('002')
+        expect(store.get('002')).to.be.undefined
+      })
+
+
+      it('decreases the length of the store by 1', function () {
+        console.log(data)
+
+        store.del('002')
+        expect(store.length()).to.eql(2)
+      })
+    })
   })
 })
